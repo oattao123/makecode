0db2bb763a6aa3ba94a458d9e33bf4d8fb5b1523
oattao123 <72804848+oattao123@users.noreply.github.com>
2021-11-15 07:32:44 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(3)
+})
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     basic.showNumber(1)
     basic.showNumber(2)
